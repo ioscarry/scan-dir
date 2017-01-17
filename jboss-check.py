f27e59e0b9f9b0e267c8d5c8934d51e23d6493c4
rcyouji7 <rcyouji7@gmail.com>
2017-01-17 18:06:03 +0800

    Update jboss-check.py

--- jboss-check.py
+++ jboss-check.py
@@ -1,86 +1,69 @@
-#coding:utf-8
-"""
-shortcoming:only test 600 ips.
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
 import urllib2
-import threading
-#import multiprocessing
 import time
 from sys import argv
+import threadpool
+
+
+"""
+python jboss-check.py ips.txt 50
+
+"""
+
+
 file = argv[1]
-#url = 'http://103.237.144.234:8080' ##http://103.237.144.234:8080/
 f  = open(file,'r')
 
 urls = f.readlines()
 f.close()
-#success = []
+
 vuls =['/jmx-console','/web-console','/invoker/JMXInvokerServlet','/admin-console']
 
-#lock = threading.Semaphore(100)
-def get_content(url,vuls):
-	#lock.acquire()
+
+def get_content(url):
 	for vul in vuls:
 		try: 
 			x = url.strip()
 			url_go = 'http://%s'%x+vul
 			print url_go
-			content = urllib2.urlopen(url_go,timeout=8)
-			html = content.read()
+			content = urllib2.urlopen(url_go,timeout=5)
+			#html = content.read()
 			code = content.code
 			#print content.url
-			w = 'http://'+url.strip()+'\r\n'
 			if code != 404:
-				if code ==401:
-					write(w)
-					break	
-				elif 'JMX Management Console' in html:  #jmx-console
-					write(w)
-					break
-				elif 'Administration Console' in html:  #web-console
-					write(w)
-					break
-				elif 'jboss' in html :					#admin-console
-					write(w)
-					break
-				elif 'jboss' in html:					#JMXInvokerServlet
-					write(w)
-					break
-				else:pass
+				w = 'http://'+url.strip()+'\n'
+				write(w)
+				break	
+
 
 		except urllib2.HTTPError,e:
-			print str(e.code)+' HTTPError'
+			print str(e.code)+url_go+' HTTPError'
 			continue
 
-		except Exception,e:   ### timeout
+		except Exception,e: ### timeout
 			print e
-			continue
+			break
 
 		except urllib2.URLError,e:
-			print str(e.code)+' URLError'
-			continue
+			print str(e.code)+url_go+' URLError'
+			break
 
-	#lock.release()
+
 def write(url):
 	with open('success.txt','a') as f:
 		f.write(url)
-	print 'success one - -'
+		print 'success one - -'
 
 
 
-threads=[]
+pool = threadpool.ThreadPool(argv[2])  
+
 start = time.time()
 
-#with open(file,'r') as urls:
-for url in urls:
-	t = threading.Thread(target=get_content,args=(url,vuls))
-	threads.append(t)
-
-if __name__=='__main__':
-	for i in range(len(threads)):
-		threads[i].start()
-		#threads[i].join()
-	for i in range(len(threads)):
-		threads[i].join()
+requests = threadpool.makeRequests(get_content, urls)
+[pool.putRequest(req) for req in requests] 
+pool.wait()  
 
 print 'Need '+str(time.time()-start)
-
